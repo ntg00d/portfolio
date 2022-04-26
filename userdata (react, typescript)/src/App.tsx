c4fb1604e36669b4bba2e37400f8d47ff3f2330a
Nick <99430009+ntg00d@users.noreply.github.com>
2022-04-26 19:12:48 +0300

    portfolio

--- userdata (react, typescript)/src/App.tsx
+++ userdata (react, typescript)/src/App.tsx
@@ -1,17 +1,27 @@
 import {useState, useEffect} from 'react'
 import UsersItem from './components/UsersItem'
+import Spinner from './components/Spinner'
 import {TApp, IUser} from './types/App'
 import './App.css'
 
 const App: TApp = () => {
   const [users, setUsers] = useState<IUser[]>([])
+  const [loadState, setLoadState] = useState<boolean | string>(false)
   const [searchName, setSearchName] = useState('')
   const [searchCity, setSearchCity] = useState('')
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(data => setUsers(data))
+    setLoadState(true)
+    setTimeout(async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        setUsers(data)
+        setLoadState(false)
+      } catch (error) {
+        console.log(error)
+      }
+    }, 1000)
   }, [])
 
   return (
@@ -26,14 +36,17 @@ const App: TApp = () => {
             </div>
           </div>
         </div>
+
         <div className="users">
-          <div className="users__title">Список пользователей</div>
-          <div className="users__list">
-            {users.filter(el => {
-              const template = (type: string, word: string) => type.toLowerCase().includes(word.toLowerCase())
-              return template(el.name, searchName) && template(el.address.city, searchCity) && el
-            }).map(el => <UsersItem key={el.id} el={el}/>)}
-          </div>
+          {loadState ? <div className="load-spinner"><Spinner/></div> : <>
+            <div className="users__title">Список пользователей</div>
+            <div className="users__list">
+              {users.filter(el => {
+                const template = (type: string, word: string) => type.toLowerCase().includes(word.toLowerCase())
+                return template(el.name, searchName) && template(el.address.city, searchCity) && el
+              }).map(el => <UsersItem key={el.id} el={el}/>)}
+            </div>
+          </>}
         </div>
 
       </div>
